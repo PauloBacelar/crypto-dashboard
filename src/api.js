44dbcd474a18a30728e13f669878e803dd68800b
Paulo Andrade <paulobacelara@gmail.com>
2021-12-12 14:47:49 -0300

    Changed to CoinGecko API and created getCoinHistory function

--- src/api.js
+++ src/api.js
@@ -1,30 +1,46 @@
+// Imports
 import axios from "axios";
 
+// Constants
 const API_KEY = "859327c415mshc26c9950ac5fccap19b7dejsn0945ec15cc45";
+const BASE_URL = "https://coingecko.p.rapidapi.com";
 
-const fetchData = (func) => {
-  var options = {
+// Fetch function
+const fetchData = (endpoint, params) => {
+  const options = {
     method: "GET",
-    url: "https://alpha-vantage.p.rapidapi.com/query",
-    params: {
-      market: "USD",
-      symbol: "BTC",
-      function: func,
-    },
+    url: BASE_URL + endpoint,
+    params,
     headers: {
-      "x-rapidapi-host": "alpha-vantage.p.rapidapi.com",
-      "x-rapidapi-key": API_KEY,
+      "x-rapidapi-host": "coingecko.p.rapidapi.com",
+      "x-rapidapi-key": "cab2c8b3a3msh3d06a5d178eb54ep1ff524jsnba143026a970",
     },
   };
 
   axios
     .request(options)
     .then((response) => {
-      console.log(response.data);
+      return response.data;
     })
     .catch((error) => {
-      console.log(error);
+      return error;
     });
 };
 
-fetchData("DIGITAL_CURRENCY_DAILY");
+const data = {
+  getHomeData: async (currency) =>
+    await fetchData("/coins/markets", {
+      vs_currency: currency,
+      page: "1",
+      per_page: "100",
+      order: "market_cap_desc",
+    }),
+
+  getCoinHistory: async (coinID, from, to, currency) => {
+    await fetchData(`/coins/${coinID}/market_chart/range`, {
+      from,
+      vs_currency: currency,
+      to,
+    });
+  },
+};
