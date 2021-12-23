e3f1d9802686b5d9b35ef98f160e97fab7fd2704
Paulo Andrade <paulobacelara@gmail.com>
2021-12-23 08:28:48 -0300

    Added colors to graph (now it is visible

--- src/components/UI/Modal/Chart/Chart.js
+++ src/components/UI/Modal/Chart/Chart.js
@@ -2,8 +2,6 @@ import "chart.js/auto";
 import { Chart } from "react-chartjs-2";
 
 const DataChart = (props) => {
-  console.log(props.yLabels);
-
   return (
     <div id="chart">
       <Chart
@@ -16,6 +14,11 @@ const DataChart = (props) => {
               id: 1,
               label: "",
               data: props.yLabels,
+              backgroundColor: "rgba(16, 100, 42, .5)",
+              fill: true,
+              pointRadius: 1.5,
+              borderColor: "rgb(16, 200, 42)",
+              borderWidth: 2,
             },
           ],
         }}
